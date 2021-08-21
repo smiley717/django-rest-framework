6c3a83ec5c267e42b2805814d376dee3ced1871c
smiley717 <smiley.dev717@gmail.com>
2021-08-21 13:50:37 -0500

    Initialize django rest framework project

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializers, GroupSerializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializers
+    permission_classes = [permissions.IsAuthenticated]
